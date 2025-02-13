6e2659a320b20f2696528b76c683c8e5515b4717
Giseldo <giseldo@gmail.com>
2025-02-12 22:29:03 -0300

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/study/',
   title: "Gerador de Plano de Estudos",
   description: "Um gerador de plano de estudos",
   themeConfig: {
